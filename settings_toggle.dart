f198e4316b23783c880c2c8bd89bae01119e1765
Md Mobashir Hasan <99482514+mohas8@users.noreply.github.com>
2025-04-08 02:47:35 +0600

    Create settings_toggle.dart

---
+++ settings_toggle.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SettingsScreen());
+  }
+}
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Settings")),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: Text("Dark Mode"),
+            value: isDarkMode,
+            onChanged: (value) {
+              setState(() => isDarkMode = value);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
